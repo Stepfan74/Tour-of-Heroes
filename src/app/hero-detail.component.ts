fc4f37c6ece5e0e3c7af1cc8206f7b06250d82e6
Stepfan74 <mikikvar@gmail.com>
2018-06-09 18:34:29 +0300

    add dashbord and beginig routing

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+// Keep the Input import for now, you'll remove it later:
+import 'rxjs/add/operator/switchMap';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 @Component({
@@ -6,7 +12,8 @@ import { Hero } from './hero';
   template: `
     <div *ngIf ="hero">
       <h2>{{hero.name}} details !</h2>
-<div><label>id: </label>{{hero.id}}</div>
+<div>
+  <label>id: </label>{{hero.id}}</div>
 <div>
   <label>name: </label>
 <input [(ngModel)]="hero.name" placeholder="name">
@@ -14,6 +21,20 @@ import { Hero } from './hero';
   </div>
   `
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+
 }
